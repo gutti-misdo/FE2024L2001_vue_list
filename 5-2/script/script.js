3e57efa0ca76586b567638100bff743e311a95b4
gutti-misdo <2301162@s.asojuku.ac.jp>
2024-12-16 10:54:41 +0900

    commit

--- 5-2/script/script.js
+++ 5-2/script/script.js
@@ -2,7 +2,16 @@ new Vue({
   el: '#app',
   data() {
     return {
-
+      menus: [{
+        label: 'TOP',
+        psth: './index.html'
+      },{
+        label: 'ABOUT',
+        path: './about.html'
+      },{
+        label: 'SCHEDULE',
+        path: './contact.html'
+      }]
     };
   }
 });
